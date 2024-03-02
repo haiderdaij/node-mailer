4e5ccaa8d6f6737d96d95b9970e205f349c3a9b4
Haider Daij <haiderduaij@gmail.com>
2024-03-02 19:41:28 +0300

    vercel -finalTouch

--- api/index.js
+++ api/index.js
@@ -1,9 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
 const nodemailer = require("nodemailer");
-const movies = require("./movies");
 
 const app = express();
 app.use(express.json());
@@ -16,9 +13,10 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-app.get("/", (req, res) => res.send("Hello there"));
+app.get("/", (req, res) =>
+  res.send("Hello there, this is a nodeMailer app based on SMTP.")
+);
 
-// Email Sending Route
 app.post("/send-email", async (req, res) => {
   const { to, subject, text } = req.body;
 
@@ -39,38 +37,8 @@ app.post("/send-email", async (req, res) => {
     res.status(500).send({ error: "Failed to send email" });
   }
 });
+const PORT = process.env.PORT;
 
-// const server = http.createServer(app);
-// const io = new Server(server, {
-//   cors: {
-//     origin: "*",
-//     methods: ["GET", "POST"],
-//   },
-// });
-
-// Server Listen
-const PORT = 3500;
-app.listen(PORT, () => console.log("Server ready on port 3500."));
-
-// server.listen(PORT, "0.0.0.0", () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
-
-// io.on("connection", (socket) => {
-//   console.log("New connection:", socket.id);
-//   let movieIndex = 0;
-//   const intervalId = setInterval(() => {
-//     if (movieIndex < movies.length) {
-//       socket.emit("movie", movies[movieIndex++]);
-//     } else {
-//       clearInterval(intervalId);
-//     }
-//   }, 6000);
-
-//   socket.on("disconnect", () => {
-//     console.log("User disconnected:", socket.id);
-//     clearInterval(intervalId);
-//   });
-// });
+app.listen(PORT, () => console.log(`Server ready on port ${PORT}.`));
 
 module.exports = app;
