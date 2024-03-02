ca2ba2ca112bcd14ac5b0c732646afa5ca7503ab
Haider Daij <haiderduaij@gmail.com>
2024-03-02 04:25:40 +0300

    vercel -node test2

--- src/index.js
+++ src/index.js
@@ -45,7 +45,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: [`http://${IP_ADDRESS}:3000`, "http://localhost:3000"],
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
