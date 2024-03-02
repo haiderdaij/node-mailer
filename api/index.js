2923e4bbd1bfa23bc0af69d4bca9bfa5fff46571
Haider Daij <haiderduaij@gmail.com>
2024-03-02 06:39:20 +0300

    vercel --test

--- api/index.js
+++ api/index.js
@@ -1,25 +1,21 @@
-// Configurations and Imports
-require("dotenv").config();
+// require("dotenv").config();
 const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
-const nodemailer = require("nodemailer");
-const movies = require("./movies").movies;
+// const http = require("http");
+// const { Server } = require("socket.io");
+// const nodemailer = require("nodemailer");
+// const movies = require("./movies");
 
-// Initialize Express App
 const app = express();
-app.use(express.json());
+// app.use(express.json());
 
-// Email Transporter Configuration
-const transporter = nodemailer.createTransport({
-  service: process.env.SERVICE_TYPE,
-  auth: {
-    user: process.env.USER_NAME,
-    pass: process.env.PASS_WORD,
-  },
-});
+// const transporter = nodemailer.createTransport({
+//   service: process.env.SERVICE_TYPE,
+//   auth: {
+//     user: process.env.USER_NAME,
+//     pass: process.env.PASS_WORD,
+//   },
+// });
 
-// Simple Root Route
 app.get("/", (req, res) => res.send("Hello there"));
 
 // Email Sending Route
@@ -44,39 +40,37 @@ app.post("/send-email", async (req, res) => {
   }
 });
 
-// HTTP Server and Socket.IO Configuration
-const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-  },
-});
+// const server = http.createServer(app);
+// const io = new Server(server, {
+//   cors: {
+//     origin: "*",
+//     methods: ["GET", "POST"],
+//   },
+// });
 
 // Server Listen
 const PORT = 3500;
 app.listen(PORT, () => console.log("Server ready on port 3500."));
 
-server.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// server.listen(PORT, "0.0.0.0", () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
 
-// Socket.IO Connection Handling
-io.on("connection", (socket) => {
-  console.log("New connection:", socket.id);
-  let movieIndex = 0;
-  const intervalId = setInterval(() => {
-    if (movieIndex < movies.length) {
-      socket.emit("movie", movies[movieIndex++]);
-    } else {
-      clearInterval(intervalId);
-    }
-  }, 6000);
+// io.on("connection", (socket) => {
+//   console.log("New connection:", socket.id);
+//   let movieIndex = 0;
+//   const intervalId = setInterval(() => {
+//     if (movieIndex < movies.length) {
+//       socket.emit("movie", movies[movieIndex++]);
+//     } else {
+//       clearInterval(intervalId);
+//     }
+//   }, 6000);
 
-  socket.on("disconnect", () => {
-    console.log("User disconnected:", socket.id);
-    clearInterval(intervalId);
-  });
-});
+//   socket.on("disconnect", () => {
+//     console.log("User disconnected:", socket.id);
+//     clearInterval(intervalId);
+//   });
+// });
 
 module.exports = app;
